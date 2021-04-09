d6256d859b56236d7187873d7f365f8a313fd140
Bowon Ryu <bowon.ryu@samsung.com>
2021-04-09 13:59:24 +0900

    [TEST] add test for editor.GetNaturalSize() issue
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ text-natural-size/text-natural.cs
@@ -0,0 +1,149 @@
+/*
+* Copyright (c) 2019 Samsung Electronics Co., Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+using System;
+using System.Threading.Tasks;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace TextComponentsTest
+{
+    class Example : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Window window = Window.Instance;
+            window.BackgroundColor = Color.White;
+
+            View view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    LinearAlignment = LinearLayout.Alignment.Begin,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.FillToParent,
+                BackgroundColor = Color.Black,
+            };
+
+            // Test for https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-toolkit/+/253286/
+
+            // Label for title
+            TextLabel label = new TextLabel
+            {
+                Text = "TextEditor",
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+                BackgroundColor = Color.CadetBlue,
+            };
+            view.Add(label);
+            
+
+
+            // TextEditor
+            TextEditor editor = new TextEditor
+            {
+                Text = "Lorem ipsum dolor sit amet, \nconsectetur adipiscing elit, \nsed do eiusmod \ntempor \nincididunt ut \nlabore et \ndolore magna \naliqua.",
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                Size2D = new Size2D(480, 400),
+            };
+            view.Add(editor);
+
+
+
+            // Button -> editor.GetNaturalSize()
+            Button button = new Button
+            {
+                Text = "Get Natural Size",
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+            };
+
+            button.Clicked += (s, e) =>
+            {
+                var naturalSize = editor.GetNaturalSize();
+                Tizen.Log.Fatal("NUI", "Editor Natural Size W : " + naturalSize.Width + " H : " + naturalSize.Height + "\n");
+            };
+
+            view.Add(button);
+
+
+            // Label for title
+            TextLabel label2 = new TextLabel
+            {
+                Text = "TextField",
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+                BackgroundColor = Color.CadetBlue,
+            };
+            view.Add(label2);
+
+
+            // TextField
+            TextField field = new TextField
+            {
+                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+            };
+            view.Add(field);
+
+
+            // Button -> field.GetNaturalSize()
+            Button button2 = new Button
+            {
+                Text = "Get Natural Size",
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+            };
+
+            button2.Clicked += (s, e) =>
+            {
+                var naturalSize = field.GetNaturalSize();
+                Tizen.Log.Fatal("NUI", "Field Natural Size W : " + naturalSize.Width + " H : " + naturalSize.Height + "\n");
+            };
+
+            view.Add(button2);
+
+
+
+            window.Add(view);
+        }
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            example.Run(args);
+        }
+    }
+}
