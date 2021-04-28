221bff4daa4fefdafc703c608e01fa48b8b44a82
Bowon Ryu <bowon.ryu@samsung.com>
2021-04-28 15:30:55 +0900

    [TEST] add test for Text Markup, this can test <u> tag
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ text-markup/markup.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Threading.Tasks;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace TextComponentsTest
+{
+    class Example : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Window window = Window.Instance;
+            window.BackgroundColor = Color.White;
+
+            View view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    LinearAlignment = LinearLayout.Alignment.Begin,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.FillToParent,
+                BackgroundColor = Color.Black,
+            };
+
+
+            // Normal label
+            TextLabel label = new TextLabel
+            {
+                Text = "<u>Lorem ipsum</u> dolor <u>sit amet,</u> consectetur adipiscing <u>elit, <u>sed do eiusmod tempor</u> incididunt ut labore</u> et dolore magna aliqua.",
+                EnableMarkup = true,
+                MultiLine = true,
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,   // only single line
+            };
+            view.Add(label);
+
+
+            // Ellipsis label
+            TextLabel ellipsisLabel = new TextLabel
+            {
+                Text = "<u>Lorem ipsum dolor sit amet,</u> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                EnableMarkup = true,
+                MultiLine = false,
+                Ellipsis = true,
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,   // only single line
+            };
+            view.Add(ellipsisLabel);
+
+
+            // Normal field
+            TextField field = new TextField
+            {
+                Text = "Lorem ipsum dolor sit amet, <u>Underline text filed</u>",
+                EnableMarkup = true,
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+
+                PlaceholderText = "Placeholder Text",
+                PlaceholderTextColor = Color.Gray,
+                PlaceholderTextFocused = "Placeholder Text Focused",
+
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,   // only single line
+            };
+/*
+            PropertyMap underline = new PropertyMap();
+            underline.Add("enable", new PropertyValue("true"));
+            underline.Add("color", new PropertyValue(Color.Green));
+            underline.Add("height", new PropertyValue(2.0f));
+            field.Underline = underline;
+*/
+            // field.TextChanged += onTextFieldTextChanged;
+            field.TextChanged += (s, e) =>
+            {
+                Tizen.Log.Error("NUI", "Field Text Changed[" + e.TextField.Text + "] \n");
+            };
+
+            view.Add(field);
+
+
+            // Password field
+            TextField passwordField = new TextField
+            {
+                // Text = "Text Field",
+                // EnableMarkup = true,
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+
+                MaxLength = 20,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+                
+                PlaceholderText = "Password",
+                PlaceholderTextFocused = "Input Password",
+                // InputMethodSettings = (new InputMethod{PanelLayout = InputMethod.PanelLayoutType.Password}).OutputMap,
+            };
+
+            var inputMethod = new InputMethod();
+            inputMethod.PanelLayout = InputMethod.PanelLayoutType.Password;
+            passwordField.InputMethodSettings = inputMethod.OutputMap;
+
+            var hiddenMap = new PropertyMap();
+            hiddenMap.Add(HiddenInputProperty.Mode, new PropertyValue((int)HiddenInputModeType.ShowLastCharacter));
+            hiddenMap.Add(HiddenInputProperty.ShowLastCharacterDuration, new PropertyValue(500));
+            passwordField.HiddenInputSettings = hiddenMap;
+
+            view.Add(passwordField);
+
+
+            // Need to implement layout editor
+            TextEditor editor = new TextEditor
+            {
+                Text = "Lorem ipsum dolor sit amet, <u>consectetur adipiscing elit, sed do eiusmod tempor</u> incididunt ut labore et dolore <u>magna</u> aliqua.",
+                // Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                EnableMarkup = true,
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+
+                HorizontalAlignment = HorizontalAlignment.Begin,   // there is no VerticalAlignment in TextEditor
+            };
+
+            // FIXME
+            float height = editor.GetHeightForWidth(480);
+            editor.Size2D = new Size2D(480, (int)height);
+
+            // editor.TextChanged += onTextEditorTextChanged;
+            editor.TextChanged += (s, e) =>
+            {
+                Tizen.Log.Error("NUI", "Editor Text Changed[" + e.TextEditor.Text + "] \n");
+            };
+
+            view.Add(editor);
+
+
+            window.Add(view);
+        }
+
+        public void onTextFieldTextChanged(object sender, TextField.TextChangedEventArgs e)
+        {
+            Tizen.Log.Fatal("NUI", "Field Text Changed : " + e.TextField.Text + "\n");
+        }
+
+        public void onTextEditorTextChanged(object sender, TextEditor.TextChangedEventArgs e)
+        {
+            Tizen.Log.Fatal("NUI", "Editor Text Changed : " + e.TextEditor.Text + "\n");
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            example.Run(args);
+        }
+    }
+}
