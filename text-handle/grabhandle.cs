7d83dd8d3226b2bfd00a8304166386bc62cc05b7
Bowon Ryu <bowon.ryu@samsung.com>
2021-05-14 18:51:21 +0900

    [TEST] clean code for GrabHandle test..
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- text-handle/grabhandle.cs
+++ text-handle/grabhandle.cs
@@ -24,185 +24,125 @@ namespace TextComponentsTest
                 {
                     LinearOrientation = LinearLayout.Orientation.Vertical,
                     LinearAlignment = LinearLayout.Alignment.Begin,
-                    CellPadding = new Size2D(10, 10),
+                    //CellPadding = new Size2D(10, 10),
                 },
                 WidthResizePolicy = ResizePolicyType.FillToParent,
                 HeightResizePolicy = ResizePolicyType.FillToParent,
                 BackgroundColor = Color.Black,
             };
-
-
-            // Normal label
-            TextLabel label = new TextLabel
-            {
-                Text = "Lorem ipsum dolor",
-                // Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                // EnableMarkup = true,
-                MultiLine = true,
-                WidthResizePolicy = ResizePolicyType.FillToParent,
-                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
-                PointSize = 25.0f,
-                BackgroundColor = Color.White,
-                HorizontalAlignment = HorizontalAlignment.Begin,
-                VerticalAlignment = VerticalAlignment.Center,   // only single line
-            };
-            view.Add(label);
-
-
-            // Ellipsis label
-            TextLabel ellipsisLabel = new TextLabel
-            {
-                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                // EnableMarkup = true,
-                MultiLine = false,
-                Ellipsis = true,
-                WidthResizePolicy = ResizePolicyType.FillToParent,
-                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
-                PointSize = 25.0f,
-                BackgroundColor = Color.White,
-                HorizontalAlignment = HorizontalAlignment.Begin,
-                VerticalAlignment = VerticalAlignment.Center,   // only single line
-            };
-            view.Add(ellipsisLabel);
+            window.Add(view);
 
 
             // Normal field
             TextField field = new TextField
             {
-                Text = "Text Field",
-                // EnableMarkup = true,
-                Size2D = new Size2D(480, 80),
-                // WidthResizePolicy = ResizePolicyType.FillToParent,
-                // HeightResizePolicy = ResizePolicyType.UseNaturalSize,
-               
+                Text = "EnableGrabHandle = false",
+
+                Size2D = new Size2D(400, 50),               
 
                 MaxLength = 200,
-
                 BackgroundColor = Color.White,
 
-                PlaceholderText = "Placeholder Text",
-                PlaceholderTextColor = Color.Gray,
-                PlaceholderTextFocused = "Placeholder Text Focused",
-
                 EnableGrabHandle = false,
-                EnableSelection = false,
+                //EnableSelection = false,
+                EnableGrabHandlePopup = false,
 
-                HorizontalAlignment = HorizontalAlignment.Begin,
-                VerticalAlignment = VerticalAlignment.Center,   // only single line
-                PointSize = 25.0f,
+                PointSize = 20.0f,
             };
-
-            // field.TextChanged += onTextFieldTextChanged;
-            field.TextChanged += (s, e) =>
-            {
-                Tizen.Log.Error("NUI", "Field Text Changed[" + e.TextField.Text + "] \n");
-                Tizen.Log.Error("NUI", "PointSize[" + e.TextField.PointSize + "] \n");
-                Tizen.Log.Error("NUI", "EnableSelection[" + e.TextField.EnableSelection + "] \n");
-
-            };
-
             view.Add(field);
 
 
-            // Password field
-            TextField passwordField = new TextField
-            {
-                // Text = "Text Field",
-                // EnableMarkup = true,
-                WidthResizePolicy = ResizePolicyType.FillToParent,
-                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
-
-                MaxLength = 200,
-                PointSize = 25.0f,
-                BackgroundColor = Color.White,
-                
-                PlaceholderText = "Password",
-                PlaceholderTextFocused = "Input Password",
-                // InputMethodSettings = (new InputMethod{PanelLayout = InputMethod.PanelLayoutType.Password}).OutputMap,
-            };
-
-            var inputMethod = new InputMethod();
-            inputMethod.PanelLayout = InputMethod.PanelLayoutType.Password;
-            passwordField.InputMethodSettings = inputMethod.OutputMap;
-
-            var hiddenMap = new PropertyMap();
-            hiddenMap.Add(HiddenInputProperty.Mode, new PropertyValue((int)HiddenInputModeType.ShowLastCharacter));
-            hiddenMap.Add(HiddenInputProperty.ShowLastCharacterDuration, new PropertyValue(500));
-            passwordField.HiddenInputSettings = hiddenMap;
-
-            view.Add(passwordField);
-
-
-            // Need to implement layout editor
+            // editor
             TextEditor editor = new TextEditor
             {
-                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                // Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                EnableMarkup = true,
-                WidthResizePolicy = ResizePolicyType.FillToParent,
-                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
-                PointSize = 25.0f,
+                Text = "(editor)EnableSelection = false",
+
+                Size2D = new Size2D(410, 50),
+
+                PointSize = 20.0f,
                 BackgroundColor = Color.White,
 
-                //EnableGrabHandle = false,
+                // Need to implements
+                // EnableGrabHandle = false,
+                // EnableGrabHandlePopup = false,
                 EnableSelection = false,
-
-                HorizontalAlignment = HorizontalAlignment.Begin,   // there is no VerticalAlignment in TextEditor
             };
-
-            // FIXME
-            float height = editor.GetHeightForWidth(480);
-            editor.Size2D = new Size2D(480, (int)height);
-
-            // editor.TextChanged += onTextEditorTextChanged;
-            editor.TextChanged += (s, e) =>
-            {
-                Tizen.Log.Error("NUI", "Editor Text Changed[" + e.TextEditor.Text + "] \n");
-            };
-
             view.Add(editor);
 
 
             // Normal field
             TextField field2 = new TextField
             {
-                Text = "Text Field",
-                // EnableMarkup = true,
-                Size2D = new Size2D(480, 80),
-                // WidthResizePolicy = ResizePolicyType.FillToParent,
-                // HeightResizePolicy = ResizePolicyType.UseNaturalSize,
-               
+                Text = "EnableGrabHandle = false",
+                
+                Size2D = new Size2D(420, 50),               
 
                 MaxLength = 200,
                 BackgroundColor = Color.White,
 
-                PlaceholderText = "Placeholder Text",
-                PlaceholderTextColor = Color.Gray,
-                PlaceholderTextFocused = "Placeholder Text Focused",
-
                 EnableGrabHandle = false,
                 EnableSelection = false,
 
-                HorizontalAlignment = HorizontalAlignment.Begin,
-                VerticalAlignment = VerticalAlignment.Center,   // only single line
-
-
-                PointSize = 25.0f,
+                PointSize = 20.0f,
             };
             view.Add(field2);
 
 
-            window.Add(view);
-        }
+            // Normal field
+            TextField field3 = new TextField
+            {
+                Text = "!handle !selection",
+                
+                Size2D = new Size2D(430, 50),
+                
+                MaxLength = 200,
+                BackgroundColor = Color.White,
 
-        public void onTextFieldTextChanged(object sender, TextField.TextChangedEventArgs e)
-        {
-            Tizen.Log.Fatal("NUI", "Field Text Changed : " + e.TextField.Text + "\n");
-        }
+                EnableGrabHandle = false,
+                EnableSelection = false,
 
-        public void onTextEditorTextChanged(object sender, TextEditor.TextChangedEventArgs e)
-        {
-            Tizen.Log.Fatal("NUI", "Editor Text Changed : " + e.TextEditor.Text + "\n");
+                PointSize = 20.0f,
+            };
+            view.Add(field3);
+
+
+            // Control field
+            TextField field4 = new TextField
+            {
+                // Text = "!handle !selection",
+                Size2D = new Size2D(440, 50),
+               
+                MaxLength = 200,
+                BackgroundColor = Color.White,
+
+                //EnableGrabHandle = false,
+                //EnableSelection = false,
+
+                PointSize = 20.0f,
+            };
+            view.Add(field4);
+
+            field4.TextChanged += (s, e) =>
+            {
+                //Tizen.Log.Fatal("NUI", "field4 Text Changed : " + e.TextField.Text + "\n");
+
+                //field3.EnableGrabHandle = false;
+                //field3.EnableSelection = false;
+
+                if (field4.Text.Length %2 == 0)
+                {
+                    field3.EnableGrabHandle = false;
+                    field3.EnableSelection = false;
+                }
+                else
+                {
+                    field3.EnableGrabHandle = true;
+                    field3.EnableSelection = true;
+                }
+
+                Tizen.Log.Fatal("NUI", "Field 3 handle : " + field3.EnableGrabHandle + ", sel : " + field3.EnableSelection + "\n");
+            };
+
         }
 
         [STAThread]
