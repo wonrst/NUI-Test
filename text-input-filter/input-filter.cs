c36d5415dc08c2b5906b1c874c071d2998501c01
Bowon Ryu <bowon.ryu@samsung.com>
2021-05-21 14:57:06 +0900

    [TEST] InputFilter for NUI
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- text-input-filter/input-filter.cs
+++ text-input-filter/input-filter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Tizen.NUI;
 using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
 using System.Text.RegularExpressions;
 
 namespace TextComponentsTest
@@ -31,55 +32,246 @@ namespace TextComponentsTest
                 HeightResizePolicy = ResizePolicyType.FillToParent,
                 BackgroundColor = Color.Black,
             };
+            window.Add(view);
 
             // Normal field
             TextField field = new TextField
             {
-                // Text = "Text Field",
-                // EnableMarkup = true,
-                Text = "01234",
-                WidthResizePolicy = ResizePolicyType.FillToParent,
-                HeightResizePolicy = ResizePolicyType.UseNaturalSize,
+                Text = "Field",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 80,
 
-                MaxLength = 20,
+                MaxLength = 200,
                 PointSize = 25.0f,
                 BackgroundColor = Color.White,
-
-                PlaceholderText = "Placeholder Text",
-                PlaceholderTextColor = Color.Gray,
-                PlaceholderTextFocused = "Placeholder Text Focused",
-
-                HorizontalAlignment = HorizontalAlignment.Begin,
-                VerticalAlignment = VerticalAlignment.Center,   // only single line
             };
-
-            field.TextChanged += onTextFieldTextChanged;
-            /*
-            field.TextChanged += (s, e) =>
-            {
-                Tizen.Log.Error("NUI", "Field Text Changed[" + e.TextField.Text + "] \n");
-            };
-            */
-
             view.Add(field);
 
+            field.TextChanged += OnTextFieldTextChanged;
 
-            window.Add(view);
+            PropertyMap inputFilter = new PropertyMap();
+            inputFilter.Add((int)InputFilterType.Accepted, new PropertyValue("[\\d]"));
+            inputFilter.Add((int)InputFilterType.Rejected, new PropertyValue("[0-3]"));
+            // inputFilter.Add("accepted", new PropertyValue("[\\d]"));
+            // inputFilter.Add("rejected", new PropertyValue("[\\d]"));
+
+            field.InputFilter = inputFilter;
+
+            field.InputFiltered += OnTextFieldInputFiltered;
+
+/*
+            field.InputFiltered += (s, e) =>
+            {
+                Tizen.Log.Fatal("NUI", "Field Input Filtered! \n");
+            };
+*/
+
+
+            // Normal editor
+            TextEditor editor = new TextEditor
+            {
+                Text = "Editor",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 80,
+
+                MaxLength = 200,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+            };
+            view.Add(editor);
+
+            editor.TextChanged += OnTextEditorTextChanged;
+
+            PropertyMap inputFilter2 = new PropertyMap();
+            inputFilter2.Insert((int)InputFilterType.Accepted, new PropertyValue("[\\D]"));
+            inputFilter2.Insert((int)InputFilterType.Rejected, new PropertyValue("[a-gA-G]"));
+
+            editor.InputFilter = inputFilter2;
+
+            editor.InputFiltered += OnTextEditorInputFiltered;
+
+
+            TextLabel label = new TextLabel
+            {
+                Text = " ▼ Predefined Filter Test",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 30,
+                PointSize = 15.0f,
+                BackgroundColor = Color.Gray,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            view.Add(label);
+
+
+            Button digitBtn = new Button
+            {
+                Text = "Digit only",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+            };
+            view.Add(digitBtn);
+            digitBtn.Clicked += (s, e) =>
+            {
+                var inputFilter = new PropertyMap();
+                inputFilter.Add((int)InputFilterType.Accepted, new PropertyValue(@"[\d]"));
+                inputFilter.Add((int)InputFilterType.Rejected, new PropertyValue(""));
+                field.InputFilter = inputFilter;
+                editor.InputFilter = inputFilter;
+            };
+
+
+            Button wordBtn = new Button
+            {
+                Text = "Word only",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+            };
+            view.Add(wordBtn);
+            wordBtn.Clicked += (s, e) =>
+            {
+                var inputFilter = new PropertyMap();
+                inputFilter.Add((int)InputFilterType.Accepted, new PropertyValue(@"[\w]"));
+                inputFilter.Add((int)InputFilterType.Rejected, new PropertyValue(""));
+                field.InputFilter = inputFilter;
+                editor.InputFilter = inputFilter;
+            };
+
+            // Need to implenments NULL situation
+            Button nofilterBtn = new Button
+            {
+                Text = "No Filter",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+            };
+            view.Add(nofilterBtn);
+            nofilterBtn.Clicked += (s, e) =>
+            {
+                field.InputFilter = null;
+                editor.InputFilter = null;
+            };
+
+
+            TextLabel label2 = new TextLabel
+            {
+                Text = " ▼ Regular Expression Test",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 30,
+                PointSize = 15.0f,
+                BackgroundColor = Color.Gray,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            view.Add(label2);
+
+
+            View horView = new View
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Horizontal,
+                    LinearAlignment = LinearLayout.Alignment.Begin,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+            };
+            view.Add(horView);
+
+
+            TextField acceptedField = new TextField
+            {
+                Text = "",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+                PlaceholderText = "Input Accepted REGEX",
+                PlaceholderTextFocused = "Input Accepted REGEX",
+                PlaceholderTextColor = Color.White,
+                PointSize = 15.0f,
+
+                BackgroundColor = Color.LightGray,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            horView.Add(acceptedField);
+
+
+            TextField rejectedField = new TextField
+            {
+                Text = "",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+                PlaceholderText = "Input Rejected REGEX",
+                PlaceholderTextFocused = "Input Rejected REGEX",
+                PlaceholderTextColor = Color.White,
+                PointSize = 15.0f,
+
+                BackgroundColor = Color.LightGray,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            horView.Add(rejectedField);
+
+
+            Button setBtn = new Button
+            {
+                Text = "Set InputFilter by REGEX",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+            };
+            view.Add(setBtn);
+            setBtn.Clicked += (s, e) =>
+            {
+                var inputFilter = new PropertyMap();
+                inputFilter.Add((int)InputFilterType.Accepted, new PropertyValue(acceptedField.Text));
+                inputFilter.Add((int)InputFilterType.Rejected, new PropertyValue(rejectedField.Text));
+                field.InputFilter = inputFilter;
+                editor.InputFilter = inputFilter;
+            };
+
+
         }
 
-        public void onTextFieldTextChanged(object sender, TextField.TextChangedEventArgs e)
+        public void OnTextFieldInputFiltered(object sender, InputFilteredEventArgs e)
         {
-            Tizen.Log.Fatal("NUI", "Field Text Changed : " + e.TextField.Text + "\n");
-            //string result = Regex.Replace(e.TextField.Text, @"[^\w]", "");
-            //string result = Regex.Replace(e.TextField.Text, "[\\W_]", "");
+            TextField field = sender as TextField;
+            string regex;
+            if (e.Type == InputFilterType.Accepted)
+            {
+                field.InputFilter.Find((int)InputFilterType.Accepted).Get(out regex);
 
-            //Tizen.Log.Fatal("NUI", "result : " + result + "\n");
-            //e.TextField.Text = result;
+                Tizen.Log.Fatal("NUI", "[FIELD][Accepted] Only accepted follow characters " + regex + "\n");
+            }
+            else if (e.Type == InputFilterType.Rejected)
+            {
+                field.InputFilter.Find((int)InputFilterType.Rejected).Get(out regex);
+
+                Tizen.Log.Fatal("NUI", "[FIELD][Rejected] Rejected follow characters " + regex + "\n");
+            }
         }
 
-        public void onTextEditorTextChanged(object sender, TextEditor.TextChangedEventArgs e)
+        public void OnTextEditorInputFiltered(object sender, InputFilteredEventArgs e)
         {
-            Tizen.Log.Fatal("NUI", "Editor Text Changed : " + e.TextEditor.Text + "\n");
+            TextEditor editor = sender as TextEditor;
+            string regex;
+            if (e.Type == InputFilterType.Accepted)
+            {
+                editor.InputFilter.Find((int)InputFilterType.Accepted).Get(out regex);
+
+                Tizen.Log.Fatal("NUI", "[EDITOR][Accepted] Only accepted follow characters " + regex + "\n");
+            }
+            else if (e.Type == InputFilterType.Rejected)
+            {
+                editor.InputFilter.Find((int)InputFilterType.Rejected).Get(out regex);
+
+                Tizen.Log.Fatal("NUI", "[EDITOR][Rejected] Rejected follow characters " + regex + "\n");
+            }
+        }
+
+        public void OnTextFieldTextChanged(object sender, TextField.TextChangedEventArgs e)
+        {
+            Tizen.Log.Fatal("NUI", "[FIELD] Text Changed : " + e.TextField.Text + "\n");
+        }
+
+        public void OnTextEditorTextChanged(object sender, TextEditor.TextChangedEventArgs e)
+        {
+            Tizen.Log.Fatal("NUI", "[EDITOR] Text Changed : " + e.TextEditor.Text + "\n");
         }
 
         [STAThread]
