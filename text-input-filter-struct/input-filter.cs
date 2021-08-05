33d6cbcabd80648ca1945163abb3e9b52811ecf9
Bowon Ryu <bowon.ryu@samsung.com>
2021-08-05 20:50:03 +0900

    [FIX] Add Set/GetInputFilter() for TextEditor
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- text-input-filter-struct/input-filter.cs
+++ text-input-filter-struct/input-filter.cs
@@ -52,15 +52,45 @@ namespace TextComponentsTest
             inputFilter.Rejected = new Regex("[0-3]"); // reject 0, 1, 2, 3
             field.SetInputFilter(inputFilter);
             field.InputFiltered += OnInputFiltered;
+
+
+            // Normal editor
+            TextEditor editor = new TextEditor
+            {
+                Text = "Editor",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 80,
+
+                MaxLength = 200,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+            };
+            view.Add(editor);
+
+            Tizen.NUI.Text.InputFilter filter;
+            filter.Accepted = new Regex(@"[\w]"); // accept words
+            filter.Rejected = new Regex(@"[\d]"); // reject digits
+            editor.SetInputFilter(filter);
+            editor.InputFiltered += (s, e) =>
+            {
+                if (e.Type == InputFilterType.Accept)
+                {
+                    Tizen.Log.Fatal("NUI", "[Editor] only accepted follow characters " + editor.GetInputFilter().Accepted + "\n");
+                }
+                else if (e.Type == InputFilterType.Reject)
+                {
+                    Tizen.Log.Fatal("NUI", "[Editor] Rejected follow characters " + editor.GetInputFilter().Rejected + "\n");
+                }
+            };
         }
 
         public void OnInputFiltered(object sender, InputFilteredEventArgs e)
         {
-            if (e.Type == InputFilterType.Accepted)
+            if (e.Type == InputFilterType.Accept)
             {
                 Tizen.Log.Fatal("NUI", "[Accepted] Only accepted follow characters " + (sender as TextField).GetInputFilter().Accepted + "\n");
             }
-            else if (e.Type == InputFilterType.Rejected)
+            else if (e.Type == InputFilterType.Reject)
             {
                 Tizen.Log.Fatal("NUI", "[Rejected] Rejected follow characters " + (sender as TextField).GetInputFilter().Rejected + "\n");
             }
