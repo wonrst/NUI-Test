5611b9ae30e275e5f2726a377c1abb971ae81450
Bowon Ryu <bowon.ryu@samsung.com>
2021-12-09 18:12:50 +0900

    [TEST] Add test for text geometry
    - TextGeometry.GetTextSize
    - TextGeometry.GetTextPosition
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ text-geometry/geometry.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Threading.Tasks;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+using System.Collections.Generic;
+
+namespace TextComponentsTest
+{
+    class Example : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Window window = Window.Instance;
+            window.BackgroundColor = Color.White;
+
+            View view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    LinearAlignment = LinearLayout.Alignment.Begin,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                BackgroundColor = Color.Black,
+            };
+            window.Add(view);
+
+
+            // Normal label
+            TextLabel label = new TextLabel
+            {
+                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                // EnableMarkup = true,
+                MultiLine = true,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                PointSize = 25.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,   // only single line
+            };
+            view.Add(label);
+
+            var button = new Button
+            {
+                Text = "get geometry of label",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+            };
+            view.Add(button);
+            button.Clicked += (s, e) =>
+            {
+                Tizen.Log.Error("NUI", "label size[" + label.Size.Width + ", " + label.Size.Height + "] \n");
+                Tizen.Log.Error("NUI", "label natural size[" + label.NaturalSize.Width + ", " + label.NaturalSize.Height + "] \n");
+
+                List<Size2D> sizeList = TextGeometry.GetTextSize(label, 0, label.Text.Length - 1);
+                List<Position2D> positionList = TextGeometry.GetTextPosition(label, 0, label.Text.Length - 1);
+
+                Tizen.Log.Error("NUI", "sizeList count[" + sizeList.Count + "] \n");
+                for (int i = 0 ; i < sizeList.Count ; i ++)
+                {
+                    Tizen.Log.Error("NUI", "item[" + i + "] width[" + sizeList[i].Width + "] height[" + sizeList[i].Height + "] \n");
+                }
+
+                Tizen.Log.Error("NUI", "positionList count[" + positionList.Count + "] \n");
+                for (int i = 0 ; i < positionList.Count ; i ++)
+                {
+                    Tizen.Log.Error("NUI", "item[" + i + "] X[" + positionList[i].X + "] Y[" + positionList[i].Y + "] \n");
+                }
+            };
+        }
+
+        public void onTextFieldTextChanged(object sender, TextField.TextChangedEventArgs e)
+        {
+            Tizen.Log.Fatal("NUI", "Field Text Changed : " + e.TextField.Text + "\n");
+        }
+
+        public void onTextEditorTextChanged(object sender, TextEditor.TextChangedEventArgs e)
+        {
+            Tizen.Log.Fatal("NUI", "Editor Text Changed : " + e.TextEditor.Text + "\n");
+        }
+
+        public void setHandle(TextField field)
+        {
+            field.GrabHandleImage = "images/handle_down.png";
+
+            PropertyMap imageLeftMap = new PropertyMap();
+            imageLeftMap.Add("filename", new PropertyValue("images/handle_downleft.png"));
+            field.SelectionHandleImageLeft = imageLeftMap;
+
+            PropertyMap imageRightMap = new PropertyMap();
+            imageRightMap.Add("filename", new PropertyValue("images/handle_downright.png"));
+            field.SelectionHandleImageRight = imageRightMap;
+        }
+
+        public void setHandle(TextEditor editor)
+        {
+            editor.GrabHandleImage = "images/handle_down.png";
+
+            PropertyMap imageLeftMap = new PropertyMap();
+            imageLeftMap.Add("filename", new PropertyValue("images/handle_downleft.png"));
+            editor.SelectionHandleImageLeft = imageLeftMap;
+
+            PropertyMap imageRightMap = new PropertyMap();
+            imageRightMap.Add("filename", new PropertyValue("images/handle_downright.png"));
+            editor.SelectionHandleImageRight = imageRightMap;
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            example.Run(args);
+        }
+    }
+}
