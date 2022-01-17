9372f04b0075ede4999dad684ce34ea4046da6cb
Bowon Ryu <bowon.ryu@samsung.com>
2022-01-17 19:53:42 +0900

    add test for font size scale..
    StyleManager.Instance.BlockControlStyleChangeSignalConnect
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- text-font-size-scale/font_size_scale.cs
+++ text-font-size-scale/font_size_scale.cs
@@ -95,6 +95,7 @@ namespace TextComponentsTest
             {
                 Tizen.Log.Error("NUI", "FontSizeScale [" + label.FontSizeScale + "] \n");
                 Tizen.Log.Error("NUI", "UseSystemSetting [" + Tizen.NUI.FontSizeScale.UseSystemSetting + "] \n");
+                Tizen.Log.Error("NUI", "Enable [" + label.EnableFontSizeScale + "][" + field.EnableFontSizeScale + "][" + editor.EnableFontSizeScale + "] \n");
             };
 
             
@@ -212,3 +213,160 @@ namespace TextComponentsTest
         }
     }
 }
+
+
+
+/* test for target
+
+using System;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Accessibility;
+using Tizen.NUI.Components;
+
+namespace Tizen.NUI.Samples
+{
+    public class AccessibilityManagerSample : IExample
+    {
+        Size2D windowSize;
+
+        public void Activate()
+        {
+            Window window = NUIApplication.GetDefaultWindow();
+            window.BackgroundColor = Color.White;
+            windowSize = window.Size;
+
+            View view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    LinearAlignment = LinearLayout.Alignment.Begin,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                BackgroundColor = Color.Black,
+                
+            };
+            window.GetDefaultLayer().Add(view);
+
+            TextLabel label = new TextLabel
+            {
+                Text = "Lorem ipsum dolor",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 180,
+                
+                PointSize = 15.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            view.Add(label);
+
+
+            TextField field = new TextField
+            {
+                Text = "Lorem ipsum dolor",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 180,
+                
+                MaxLength = 200,
+                PointSize = 15.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            view.Add(field);
+
+
+            TextEditor editor = new TextEditor
+            {
+                Text = "Lorem ipsum dolor",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 180,
+                
+                PointSize = 15.0f,
+                BackgroundColor = Color.White,
+
+                HorizontalAlignment = HorizontalAlignment.Begin,
+            };
+            view.Add(editor);
+
+
+            label.FontSizeScale = -1;
+            field.FontSizeScale = -1;
+            editor.FontSizeScale = -1;
+
+
+            TextField field1 = new TextField
+            {
+                Text = "",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+                
+                MaxLength = 200,
+                PointSize = 15.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            view.Add(field1);
+            field1.TextChanged += (s, e) =>
+            {
+                if (field1.Text.Length == 2)
+                {
+                    StyleManager.Instance.BlockControlStyleChangeSignalConnect = true;
+                }
+                else if (field1.Text.Length == 3)
+                {
+                    StyleManager.Instance.BlockControlStyleChangeSignalConnect = false;
+                }
+                
+                Tizen.Log.Error("NUI", "RYU FontSizeScale [" + label.FontSizeScale + "] \n");
+                Tizen.Log.Error("NUI", "RYU PointSize [" + label.PointSize + "][" + field.PointSize + "][" + editor.PointSize + "] \n");
+                Tizen.Log.Error("NUI", "RYU Enable [" + label.EnableFontSizeScale + "][" + field.EnableFontSizeScale + "][" + editor.EnableFontSizeScale + "] \n");
+                Tizen.Log.Error("NUI", "RYU block control [" + StyleManager.Instance.BlockControlStyleChangeSignalConnect + "] \n");
+            };
+
+            TextField field2 = new TextField
+            {
+                Text = "",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 50,
+                
+                MaxLength = 200,
+                PointSize = 15.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            view.Add(field2);
+            field2.TextChanged += (s, e) =>
+            {
+                if (field2.Text.Length == 0)
+                {
+                    label.EnableFontSizeScale = true;
+                    field.EnableFontSizeScale = true;
+                    editor.EnableFontSizeScale = true;
+                }
+                else if (field2.Text.Length == 1)
+                {
+                    label.EnableFontSizeScale = false;
+                    field.EnableFontSizeScale = false;
+                    editor.EnableFontSizeScale = false;
+                }
+                
+                Tizen.Log.Error("NUI", "RYU FontSizeScale [" + label.FontSizeScale + "] \n");
+                Tizen.Log.Error("NUI", "RYU PointSize [" + label.PointSize + "][" + field.PointSize + "][" + editor.PointSize + "] \n");
+                Tizen.Log.Error("NUI", "RYU Enable [" + label.EnableFontSizeScale + "][" + field.EnableFontSizeScale + "][" + editor.EnableFontSizeScale + "] \n");
+                Tizen.Log.Error("NUI", "RYU block control [" + StyleManager.Instance.BlockControlStyleChangeSignalConnect + "] \n");
+            };
+        }
+
+        public void Deactivate()
+        {
+        }
+    }
+}
+
+*/
