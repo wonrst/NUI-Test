5bd29e80a0a728622f1be10b9799ba2dd6dd7b91
Bowon Ryu <bowon.ryu@samsung.com>
2022-07-07 16:00:17 +0900

    [TEST] add test for label autoscroll + default
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ text-autoscroll/autoscroll.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Threading.Tasks;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace TextComponentsTest
+{
+    class Example : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Window window = Window.Instance;
+            window.BackgroundColor = Color.White;
+
+            View view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                BackgroundColor = Color.Black,
+            };
+            window.Add(view);
+
+            string shortText = "Lorem ipsum dolor";
+            string longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+            TextLabel label = NewTextLabel(longText);
+            view.Add(label);
+
+            Button button = new Button
+            {
+                Text = "autoScroll info",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+            };
+            view.Add(button);
+            button.Clicked += (s, e) =>
+            {
+                Tizen.Log.Error("NUI", "EnableAutoScroll[" + label.EnableAutoScroll + "] \n");
+                Tizen.Log.Error("NUI", "AutoScrollLoopCount[" + label.AutoScrollLoopCount + "] \n");
+                Tizen.Log.Error("NUI", "AutoScrollGap[" + label.AutoScrollGap + "] \n");
+                Tizen.Log.Error("NUI", "AutoScrollSpeed[" + label.AutoScrollSpeed + "] \n");
+            };
+        }
+
+        public TextLabel NewTextLabel(string text)
+        {
+            var label = new TextLabel
+            {
+                Text = text,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                PointSize = 15.0f,
+                BackgroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Begin,
+                VerticalAlignment = VerticalAlignment.Center,
+                EnableAutoScroll = true,
+            };
+            return label;
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            example.Run(args);
+        }
+    }
+}
